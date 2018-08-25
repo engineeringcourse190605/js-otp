47b856e8c3600ea1fddc2e6f9fad5b4cde54a78d
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2018-08-25 17:09:05 +0800

    update source code

---
+++ src/views/Store.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="about">
+        <h1>Your random entity from store is {{storeResult}}</h1>
+        <button v-on:click="randomEntity">GoStore</button>
+    </div>
+</template>
+
+<script>
+import {mapState, mapActions} from 'vuex'
+
+export default {
+    computed: mapState({
+        storeResult: state => state.entity.entries[0].API
+    }),
+    methods: {
+        ...mapActions([
+            'randomEntity'
+        ])
+    }
+}
+</script>
---
+++ src/views/View.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="about">
+        <h1>Your random entity is {{result}}</h1>
+        <button class="go" v-on:click="go">Go</button>
+    </div>
+</template>
+
+<script>
+    import Api from '@/api'
+
+    export default {
+        data: () => ({
+            result: ''
+        }),
+        methods: {
+            go: function () {
+                return Api.randomEntity()
+                    .then(response => {
+                        console.log(response)
+                        this.result = response.data.entries[0].API;
+                        this.$toasted.show(response.data.entries[0].Link, {
+                            type: 'info',
+                            position: 'bottom-center',
+                            duration: 2000
+                        })
+                    })
+                    .catch(err => {
+                        console.log(err)
+                        this.$toasted.show(err, {
+                            type: 'error',
+                            position: 'bottom-center',
+                            duration: 2000
+                        })
+                    })
+            },
+        }
+    }
+</script>
